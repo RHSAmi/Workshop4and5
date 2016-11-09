9a58c515e5ce445b2cc5fba3c7a1e2430c44fda5
RHSAmi <tkuehl@umass.edu>
2016-11-08 21:05:58 -0500

    fb6

--- app/components/commentthread.js
+++ app/components/commentthread.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import CommentEntry from './commententry';
-
-export default class CommentThread extends React.Component{
-  render(){
-    return(
+export default class CommentThread extends React.Component {
+  render() {
+    return (
       <ul className="media-list">
-        {React.Children.map(this.props.children,function(child){
-          return(
+        {React.Children.map(this.props.children, function(child) {
+          return (
             <li className="media">
               {child}
-            </li>
+              </li>
           )
         })}
         <li className="media">
-          <CommentEntry />
+        <CommentEntry onPost={this.props.onPost} />
         </li>
-      </ul>
+        </ul>
     )
   }
 }
